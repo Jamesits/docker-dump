db562c14b19773c9713efbdb065cfd65380a1852
James Swineson <github@public.swineson.me>
2020-03-02 03:00:25 -0800

    prevent WARNING: No swap limit support

--- backup.sh
+++ backup.sh
@@ -10,7 +10,7 @@ mkdir -p backup/containers
 
 # dump system info
 date +%s%N | cut -b1-13 > backup/timestamp.txt
-docker info > backup/docker_info.txt
+docker info > backup/docker_info.txt 2>&1
 
 # dump all containers
 containers=$(docker ps --all -q)
